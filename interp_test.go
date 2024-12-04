08c999e031689c85b2eda79a8eeaf57a095b55cc
Emi <emidinh6@gmail.com>
2024-12-04 12:42:48 -0800

    wrote extended env and manually merged all the code

--- interp_test.go
+++ interp_test.go
@@ -4,49 +4,84 @@ import "testing"
 
 //example test
 
-func NumCtoV(t *testing.T){
-    got1, got2 := interp(NumC{12}, topEnv)
-    want1 := NumV{12} 
 
-    if got1 != want1 {
-        t.Errorf("got %v, wanted %v", got1, want1)
-    }
+func TestNumCtoV(t *testing.T) {
+	topEnv := []Binding{
+		{"+", PrimV{"+"}},
+		{"-", PrimV{"-"}},
+		{"*", PrimV{"*"}},
+		{"/", PrimV{"/"}},
+		{"<=", PrimV{"<="}},
+		{"error", PrimV{"error"}},
+		{"equal?", PrimV{"equal?"}},
+		{"true", BoolV{true}},
+		{"false", BoolV{false}},
+		{"println", PrimV{"println"}},
+		{"read-num", PrimV{"read-num"}},
+		{"read-str", PrimV{"read-str"}},
+		{"++", PrimV{"++"}},
+	}
+	got1, got2 := interp(NumC{12}, topEnv)
+	want1 := NumV{12}
 
-    if got2 !=  nil {
-        t.Errorf("got %v, wanted %v", got2, nil)
-    }
+	if got1 != want1 {
+		t.Errorf("got %v, wanted %v", got1, want1)
+	}
+
+	if got2 != nil {
+		t.Errorf("got %v, wanted %v", got2, nil)
+	}
 }
 
-func StrCtoV(t *testing.T){
-    got1, got2 := interp(StrC{"hi"}, topEnv)
-    want := StrV{"hi"}
-
-    if got1 != want {
-        t.Errorf("got %v, wanted %v", got1, want)
-    }
-
-    if got2 != nil {
-        t.Errorf("got %v, wanted %v", got2, nil)
-    }
+func TestInterpNumC(t *testing.T) {
+	expr := NumC{42}
+	expectedValue := NumV{42}
+	env := Env{}
+	result, err := interp(expr, env)
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+	if result != expectedValue {
+		t.Errorf("Expected %v, got %v", expectedValue, result)
+	}
 }
 
-func IfC_Lookup(t *testing.T){
-    got1, got2 := interp(IdC{"a"}, topEnv)
-    want := NumC {12}
-
-    if got1 != want {
-        t.Errorf("got %v, wanted %v", got1, want)
-    }
-    if got2 != nil {
-        t.Errorf("got %v, wanted %v", got2, nil)
-    }
+func TestInterpStrC(t *testing.T) {
+	expr := StrC{str: "go!"}
+	expectedValue := StrV{str: "go!"}
+	env := Env{}
+	result, err := interp(expr, env)
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+	if result != expectedValue {
+		t.Errorf("Expected %v, got %v", expectedValue, result)
+	}
 }
 
-// func LamCtoLamV(t *testing.T){
-//     got := interp((LamC ((IdC 'a')), (AppC '+', ((IdC 'a'), (NumC 2)))), topEnv)
-//     want := (CloV ['a'],  )
-
-//     if got != want {
-//         t.Errorf("got %q, wanted %q", got, want)
-//     }
-// }
+func TestInterpIdC(t *testing.T) {
+	expr := IdC{name: "+"}
+	expectedValue := PrimV{operand: "+"}
+	topEnv := []Binding{
+		{"+", PrimV{"+"}},
+		{"-", PrimV{"-"}},
+		{"*", PrimV{"*"}},
+		{"/", PrimV{"/"}},
+		{"<=", PrimV{"<="}},
+		{"error", PrimV{"error"}},
+		{"equal?", PrimV{"equal?"}},
+		{"true", BoolV{true}},
+		{"false", BoolV{false}},
+		{"println", PrimV{"println"}},
+		{"read-num", PrimV{"read-num"}},
+		{"read-str", PrimV{"read-str"}},
+		{"++", PrimV{"++"}},
+	}
+	result, err := interp(expr, topEnv)
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+	if result != expectedValue {
+		t.Errorf("Expected %v, got %v", expectedValue, result)
+	}
+}
